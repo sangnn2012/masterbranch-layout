12f7abb2bc1571a4cb7d077f831855b8d496f9a9
Sang Nguyen <sangnn2012@gmail.com>
2021-06-07 18:43:26 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <div>Reliable, efficient delivery</div>
+      <div>Powered by Technology</div>
+      <div>
+        Our Artificial Intelligence powered tools use millions of project data
+        <br/>
+        points to ensure that your project is successful
+      </div>
+    </div>
+    <Category/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Category from '@/components/Category.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Category
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
