cf97c6a34d61dfdbd1a5a453eeb2dd83e7d9b7b4
Sang Nguyen <sangnn2012@gmail.com>
2021-06-07 21:48:33 +0700

    Finish mobile layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,124 @@
 <template>
   <div id="app">
-    <div class="header">
-      <div>Reliable, efficient delivery</div>
-      <div>Powered by Technology</div>
-      <div>
-        Our Artificial Intelligence powered tools use millions of project data
-        <br/>
-        points to ensure that your project is successful
+    <div class="app-wrapper">
+      <div class="header">
+        <div class="header__main header__main-thin">
+          Reliable, efficient delivery
+        </div>
+        <div class="header__main header__main-bold">Powered by Technology</div>
+        <div class="description" v-html="description"></div>
+      </div>
+      <div class="cards-wrapper">
+        <Card
+          v-for="card of cards"
+          :key="`card-${card.title}`"
+          :color="card.color"
+          :title="card.title"
+          :description="card.description"
+        />
       </div>
     </div>
-    <Category/>
   </div>
 </template>
 
 <script>
-import Category from '@/components/Category.vue'
+import Card from "@/components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Category
-  }
-}
+    Card,
+  },
+  data() {
+    return {
+      description: `Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful`,
+      cards: [
+        {
+          color: "cyan",
+          title: "Supervisor",
+          description: "Monitors activity to identify project roadblocks",
+        },
+        {
+          color: "red",
+          title: "Team Builder",
+          description:
+            "Scans our talent network to create the optimal team for your project",
+        },
+        {
+          color: "orange",
+          title: "Karma",
+          description: "Regularly evaluates our talents to ensure quality",
+        },
+        {
+          color: "blue",
+          title: "Calculator",
+          description:
+            "Uses data from past projects to provide better delivery estimates",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  overflow-x: hidden;
+  font-family: "Poppins", sans-serif;
+  font-size: 15px;
+  font-weight: 400;
+}
+
+#app {
+  background-color: hsl(0, 0%, 98%);
+  color: hsl(229, 6%, 66%);
+  width: 100%;
+  height: 100%;
+  .app-wrapper {
+    padding: 40px 35px;
+    .header {
+      text-align: center;
+      &__main {
+        font-size: 2rem;
+        color: hsl(234, 12%, 34%);
+        &-thin {
+          font-weight: 200;
+        }
+        &-bold {
+          font-weight: 600;
+          margin-top: 5px;
+        }
+      }
+      .description {
+        margin-top: 12px;
+      }
+    }
+    .cards-wrapper {
+      display: flex;
+      margin-top: 30px;
+    }
+  }
+}
+@media (max-width: 375px) {
+  #app {
+    .app-wrapper {
+      .header {
+        &__main {
+          font-size: 1.3rem;
+        }
+        .description {
+          font-size: 0.8rem;
+        }
+      }
+      .cards-wrapper {
+        display: flex;
+        flex-direction: column;
+      }
+    }
+  }
+}
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,79 @@
+<template>
+    <div
+        class="card-wrapper"
+        :style="`border-top: 5px solid ${colorHash[color]};`">
+        <div class="title"> {{ title }}</div>
+        <div class="description">{{ description }}</div>
+        <img :src="getImageUrl" :alt="title">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props: {
+        color: {
+            type: String,
+            require: true,
+        },
+        title: {
+            type: String,
+            require: true,
+        },
+        description: {
+            type: String,
+            require: true,
+        }
+    },
+    data() {
+        return {
+            colorHash: {
+                red: 'hsl(0, 78%, 62%)',
+                cyan: 'hsl(180, 62%, 55%)',
+                orange: 'hsl(34, 97%, 64%)',
+                blue: 'hsl(212, 86%, 64%)'
+            }
+        }
+    },
+    computed: {
+        getImageUrl() {
+            const iconName = this.title.split(' ').map(word => word.toLowerCase()).join('-');
+            return require(`@/assets/icon-${iconName}.svg`)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .card-wrapper {
+        background-color: #fff;
+        border-radius: 5px;
+        padding: 20px 15px;
+        margin-top: 20px;
+        flex: 1 1 0px;
+        .title {
+            color: hsl(234, 12%, 34%);
+            font-weight: 600;
+            font-size: 1.5rem;
+        }
+        .description {
+            margin-top: 10px;
+            font-size: 0.8rem;
+        }
+        img {
+            margin-top: 25px;
+            width: 50px;
+            float: right;
+        }
+    }
+    @media (max-width: 375px) {
+        .card-wrapper {
+            .title {
+                font-size: 1.1rem;
+            }
+            .description {
+                font-size: 0.7rem;
+            }
+        }
+    }
+</style>
